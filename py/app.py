a1c9b4002a604a64ae53a3ca1c49dad92bd1c6a8
Marla Blanco <m_blanco444@yahoo.com>
2023-09-06 20:51:03 -0700

    updated app

--- py/app.py
+++ py/app.py
@@ -70,6 +70,60 @@ def temperature_by_year():
             """
     return make_json_response(run_sql_command(query))
 
+
+@app.route("/")
+def welcome1():
+    """Returns available route"""
+    return (
+        "Welcome to Marla's Temperature API!<br/>"
+        "Available Routes:<br/>"
+        "/api/v1.0/temperature-by-year<br/>"
+    )
+
+@app.route("/api/v1.0/temperature-by-year")
+def temp_by_year() -> Response:
+    """Return the temperature anomaly data as a JSON Response"""
+    query = """
+            SELECT year, Anomaly COUNT(DISTINCT(month))
+            FROM temperature_anomalies
+            GROUP BY year, Anomaly
+            ORDER BY year
+        """
+    return make_json_response(run_sql_command(query))
+
+# flask route for retrieving temperature anomaly data
+@app.route("/api/v1.0/temperature-anomalies", methods=["GET"])
+def temp_anomalies() -> Response:
+    """returns temperature anomaly data with filtering/aggregation options"""
+
+    # retrieve query parameters from request
+    args = request.args
+    start_year = int(args.get("start_year")) if args.get("start_year") else None
+    end_year = int(args.get("end_year")) if args.get("end_year") else None
+    group_by = args.get("group_by")
+    order_by = args.get("order_by")
+
+    # construct the SQL query
+    query = f"SELECT year, month, Anomaly FROM temperature_anomalies WHERE 1"
+
+    if start_year:
+        query += f" AND year >= {start_year}"
+
+    if end_year:
+        query += f" AND year <= {end_year}"
+
+    if group_by:
+        query += f" GROUP BY {group_by}"
+
+    if order_by:
+        query += f" ORDER BY {order_by}"
+
+    # execute SQL query and retrieve data
+    data = run_sql_command(query)
+
+    # return the data as JSON response
+    return make_json_response(data)
+
 #################################################
 # Helper Functions
 #################################################
